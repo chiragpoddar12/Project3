2628e96148f276aea8a234035a43b5036af216e6
chiragpoddar12 <chiragpoddar12@gmail.com>
2017-07-03 00:37:09 +0530

    Catalog Items

--- Catalog_items.py
+++ Catalog_items.py
@@ -20,35 +20,35 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 # Create dummy category
-# category = Category(name="Snowboarding")
-# session.add(category)
-# session.commit()
+category = Category(name="Snowboarding")
+session.add(category)
+session.commit()
 
-# category = Category(name="Soccer")
-# session.add(category)
-# session.commit()
+category = Category(name="Soccer")
+session.add(category)
+session.commit()
 
-# category = Category(name="Basketball")
-# session.add(category)
-# session.commit()
+category = Category(name="Basketball")
+session.add(category)
+session.commit()
 
-# category = Category(name="Baseball")
-# session.add(category)
-# session.commit()
+category = Category(name="Baseball")
+session.add(category)
+session.commit()
 
-# category = Category(name="Foosball")
-# session.add(category)
-# session.commit()
+category = Category(name="Foosball")
+session.add(category)
+session.commit()
 
-# category = Category(name="Cricket")
-# session.add(category)
-# session.commit()
+category = Category(name="Cricket")
+session.add(category)
+session.commit()
 
 
 # Create dummy item
-# item = Item(user_id=1, category_id = 1, name="Goggles", created_on=datetime.datetime.now())
-# session.add(item)
-# session.commit()
+item = Item(user_id=1, category_id = 1, name="Goggles", created_on=datetime.datetime.now())
+session.add(item)
+session.commit()
 
 # Create dummy category
 # category = Category(name="Baseball")
@@ -56,98 +56,98 @@ session = DBSession()
 # session.commit()
 
 # Create dummy item
-# item = Item(user_id=1,
-#             category_id = 2,
-#             name="Gloves",
-#             created_on=datetime.datetime.now(),
-#             desc = "desc1")
-# session.add(item)
-# session.commit()
+item = Item(user_id=1,
+            category_id = 2,
+            name="Gloves",
+            created_on=datetime.datetime.now(),
+            desc = "desc1")
+session.add(item)
+session.commit()
 
-# item = Item(user_id=1,
-#             category_id = 3,
-#             name="Helmet",
-#             created_on=datetime.datetime.now(),
-#             desc = "desc2")
-# session.add(item)
-# session.commit()
+item = Item(user_id=1,
+            category_id = 3,
+            name="Helmet",
+            created_on=datetime.datetime.now(),
+            desc = "desc2")
+session.add(item)
+session.commit()
 
-# item = Item(user_id=1,
-#             category_id = 1,
-#             name="Snowboard",
-#             created_on=datetime.datetime.now(),
-#             desc = 'desc3')
-# session.add(item)
-# session.commit()
+item = Item(user_id=1,
+            category_id = 1,
+            name="Snowboard",
+            created_on=datetime.datetime.now(),
+            desc = 'desc3')
+session.add(item)
+session.commit()
 
-# item = Item(user_id=1,
-#             category_id = 4,
-#             name="Jacket",
-#             created_on=datetime.datetime.now(),
-#             desc = 'desc4')
-# session.add(item)
-# session.commit()
+item = Item(user_id=1,
+            category_id = 4,
+            name="Jacket",
+            created_on=datetime.datetime.now(),
+            desc = 'desc4')
+session.add(item)
+session.commit()
 
-# item = Item(user_id=1,
-#             category_id = 5,
-#             name="Gloves",
-#             created_on=datetime.datetime.now(),
-#             desc = "desc1")
-# session.add(item)
-# session.commit()
+item = Item(user_id=1,
+            category_id = 5,
+            name="Gloves",
+            created_on=datetime.datetime.now(),
+            desc = "desc1")
+session.add(item)
+session.commit()
 
-# item = Item(user_id=1,
-#             category_id = 6,
-#             name="Helmet",
-#             created_on=datetime.datetime.now(),
-#             desc = "desc2")
-# session.add(item)
-# session.commit()
+item = Item(user_id=1,
+            category_id = 6,
+            name="Helmet",
+            created_on=datetime.datetime.now(),
+            desc = "desc2")
+session.add(item)
+session.commit()
 
-# item = Item(user_id=1,
-#             category_id = 2,
-#             name="Helmet",
-#             created_on=datetime.datetime.now(),
-#             desc = 'desc3')
-# session.add(item)
-# session.commit()
+item = Item(user_id=1,
+            category_id = 2,
+            name="Helmet",
+            created_on=datetime.datetime.now(),
+            desc = 'desc3')
+session.add(item)
+session.commit()
 
-# item = Item(user_id=1,
-#             category_id = 1,
-#             name="Helmet",
-#             created_on=datetime.datetime.now(),
-#             desc = 'desc4')
-# session.add(item)
-# session.commit()
+item = Item(user_id=1,
+            category_id = 1,
+            name="Helmet",
+            created_on=datetime.datetime.now(),
+            desc = 'desc4')
+session.add(item)
+session.commit()
 
-# item = Item(user_id=1,
-#             category_id = 2,
-#             name="Gloves",
-#             created_on=datetime.datetime.now(),
-#             desc = "desc1")
-# session.add(item)
-# session.commit()
+item = Item(user_id=1,
+            category_id = 2,
+            name="Gloves",
+            created_on=datetime.datetime.now(),
+            desc = "desc1")
+session.add(item)
+session.commit()
 
-# item = Item(user_id=1,
-#             category_id = 6,
-#             name="Helmet",
-#             created_on=datetime.datetime.now(),
-#             desc = "desc2")
-# session.add(item)
-# session.commit()
+item = Item(user_id=1,
+            category_id = 6,
+            name="Helmet",
+            created_on=datetime.datetime.now(),
+            desc = "desc2")
+session.add(item)
+session.commit()
 
-# item = Item(user_id=1,
-#             category_id = 5,
-#             name="Helmet",
-#             created_on=datetime.datetime.now(),
-#             desc = 'desc3')
-# session.add(item)
-# session.commit()
+item = Item(user_id=1,
+            category_id = 5,
+            name="Helmet",
+            created_on=datetime.datetime.now(),
+            desc = 'desc3')
+session.add(item)
+session.commit()
 
-# item = Item(user_id=1,
-#             category_id = 4,
-#             name="Helmet",
-#             created_on=datetime.datetime.now(),
-#             desc = 'desc4')
-# session.add(item)
-# session.commit()
+item = Item(user_id=1,
+            category_id = 4,
+            name="Helmet",
+            created_on=datetime.datetime.now(),
+            desc = 'desc4')
+session.add(item)
+session.commit()
